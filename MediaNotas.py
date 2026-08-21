7841c597e519136c43aba2e80c923958939bda00
matheuscanejo06-hash <matheuscanejo06@gmail.com>
2026-08-20 21:20:19 -0300

    Fix sum calculation in calcular_media function

--- MediaNotas.py
+++ MediaNotas.py
@@ -1,9 +1,10 @@
 def calcular_media(notas):
     soma = 0
     for nota in notas:
-        soma = nota
+        soma += nota
     media = soma / len(notas)
     return media
+
 
 notas = [8, 7, 9, 6, 10]
 print(f"A média é: {calcular_media(notas)}")
